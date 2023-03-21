1c4c9f0fecfdce9fbed5f16d7e94d233ebeb495b
Terényi János <terenyi.janos96@gmail.com>
2023-03-22 00:44:55 +0100

    listaElemTorlese eljárás létrehozása, kódformázás

--- script.js
+++ script.js
@@ -8,29 +8,58 @@ const KUTYAK = [
   { id: 6, nev: "Pamacs", fajta: "pumi", nem: "szuka" },
 ];
 
-let kartyak
+let kartyak;
 
 function init() {
-  kartyak = document.querySelector(".kartyak")
+  kartyak = document.querySelector(".kartyak");
   kartyakLetrehozasa();
 }
 
 function kartyakLetrehozasa() {
   for (let i = 0; i < KUTYAK.length; i++) {
     const KARTYA = gyerekElemLetrehozo(kartyak, "div", "kartya");
-    const KARTYA_FEJ = gyerekElemLetrehozo(KARTYA, "div", "kartya-fejlec")
+    const KARTYA_FEJ = gyerekElemLetrehozo(KARTYA, "div", "kartya-fejlec");
     const CIM = gyerekElemLetrehozo(KARTYA_FEJ, "h3");
-    const KARTYA_TARTALOM = gyerekElemLetrehozo(KARTYA, "div", "kartya-tartalom")
-    const KARTYA_ADATOK_LISTA = gyerekElemLetrehozo(KARTYA_TARTALOM, "ul", "kartya-adatok");
-    const TORLO_GOMB = gyerekElemLetrehozo(KARTYA_TARTALOM, "button", "torlo-gomb")
+    const KARTYA_TARTALOM = gyerekElemLetrehozo(
+      KARTYA,
+      "div",
+      "kartya-tartalom"
+    );
+
+    const KARTYA_ADATOK_LISTA = gyerekElemLetrehozo(
+      KARTYA_TARTALOM,
+      "ul",
+      "kartya-adatok"
+    );
+
+    const TORLO_GOMB = gyerekElemLetrehozo(
+      KARTYA_TARTALOM,
+      "button",
+      "torlo-gomb"
+    );
+
+    KARTYA.setAttribute("index", i + 1);
 
     for (const KEY in KUTYAK[i]) {
       const LI = gyerekElemLetrehozo(KARTYA_ADATOK_LISTA, "li");
       LI.innerText = `${KEY}: ${KUTYAK[i][KEY]}`;
     }
-    CIM.innerText = `KÁRTYA #${i+1}`
-    TORLO_GOMB.innerText = "Törlés"
+
+    CIM.innerText = `KÁRTYA #${i + 1}`;
+    TORLO_GOMB.innerText = "Törlés";
+    TORLO_GOMB.addEventListener("click", listaElemTorlese);
   }
+}
+
+function listaElemTorlese(e) {
+  let szulo = e.target.parentNode.parentNode;
+  let azon = szulo.attributes["index"].value;
+
+  let i = 0;
+  while (KUTYAK[i].id != azon) i++;
+
+  KUTYAK.splice(i, 1);
+  szulo.remove();
 }
 
 function gyerekElemLetrehozo(szulo, gyerek, ...osztalyok) {
