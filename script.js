6db13fc719d706924c906e9248dec7b8c85d4164
Terényi János <terenyi.janos96@gmail.com>
2023-03-21 20:38:40 +0100

    KUTYAK objektum-lista létrehozása

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 window.addEventListener("load", init);
+const KUTYAK = [
+  { nev: "kutya1", fajta: "fajta1", nem: "szuka" },
+  { nev: "kutya2", fajta: "fajta2", nem: "kan" },
+  { nev: "kutya3", fajta: "fajta3", nem: "kan" },
+  { nev: "kutya4", fajta: "fajta4", nem: "szuka" },
+  { nev: "kutya5", fajta: "fajta5", nem: "kan" },
+  { nev: "kutya6", fajta: "fajta6", nem: "szuka" },
+];
 
 function init() {
   
