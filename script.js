7d79baca6503c7710ac519335be3e08fa5de7bc8
Terényi János <terenyi.janos96@gmail.com>
2023-03-21 22:25:27 +0100

    kartyakLetrehozasa eljárás létrehozása, kártyák formázása

--- script.js
+++ script.js
@@ -8,8 +8,29 @@ const KUTYAK = [
   { nev: "kutya6", fajta: "fajta6", nem: "szuka" },
 ];
 
+let body;
+
 function init() {
-  
+  body = document.querySelector("body");
+  kartyakLetrehozasa();
+}
+
+function kartyakLetrehozasa() {
+  for (let i = 0; i < KUTYAK.length; i++) {
+    const KARTYA = gyerekElemLetrehozo(body, "div", "kartya");
+    const KARTYA_FEJ = gyerekElemLetrehozo(KARTYA, "div", "kartya-fejlec")
+    const CIM = gyerekElemLetrehozo(KARTYA_FEJ, "h2");
+    const KARTYA_TARTALOM = gyerekElemLetrehozo(KARTYA, "div", "kartya-tartalom")
+    const KARTYA_ADATOK_LISTA = gyerekElemLetrehozo(KARTYA_TARTALOM, "ul", "kartya-adatok");
+    const TORLO_GOMB = gyerekElemLetrehozo(KARTYA_TARTALOM, "button", "torlo-gomb")
+
+    for (const KEY in KUTYAK[i]) {
+      const LI = gyerekElemLetrehozo(KARTYA_ADATOK_LISTA, "li");
+      LI.innerText = `${KEY}: ${KUTYAK[i][KEY]}`;
+    }
+    CIM.innerText = `${i+1}. KÁRTYA`
+    TORLO_GOMB.innerText = "Törlés"
+  }
 }
 
 function gyerekElemLetrehozo(szulo, gyerek, ...osztalyok) {
